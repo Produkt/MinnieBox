3cf8c790950ba851128f2cdfbc19deb4d965b87f
Daniel García <fillito@gmail.com>
2015-01-29 21:13:20 +0100

    delete invalid tests

--- MinnieDiskTests/ListContentInteractorTests.m
+++ MinnieDiskTests/ListContentInteractorTests.m
@@ -22,42 +22,13 @@
     [super setUp];
     self.dbRestClientMock = mock([DBRestClient class]);
     self.listContentInteractor = [[ListContentInteractor alloc] init];
-//    self.listContentInteractor.dbRestClient = self.dbRestClientMock;
+    self.listContentInteractor.dbRestClient = self.dbRestClientMock;
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
-
-- (void)testDeltaRequest {
-    [self.listContentInteractor listDropboxTreeWithCompletion:^(id<InodeRepresentationProtocol> inode) {
-
-    }progress:^(NSUInteger processedItems, NSUInteger total, MDLoadProgressState progressState) {
-        
-    }];
-    while (YES) {
-        [[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
-    }
-}
-
-//- (void)testDBRestClientList {
-//    [self.listContentInteractor listRootContentWithCompletion:^(id<InodeRepresentationProtocol> inode) {
-//        
-//    }];
-//    [MKTVerify(self.dbRestClientMock) loadMetadata:@"/"];
-//    
-//    
-//    id<InodeRepresentationProtocol> inode = mockProtocol(@protocol(InodeRepresentationProtocol));
-//    [given([inode inodePath]) willReturn:@"/folder"];
-//    
-//    [self.listContentInteractor listRootContentWithInode:inode withCompletion:^(id<InodeRepresentationProtocol> inode) {
-//        
-//    }];
-//    [MKTVerify(self.dbRestClientMock) loadMetadata:[inode inodePath]];
-//    
-//}
-
 
 
 @end
